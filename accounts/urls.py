7e032cf5e09954498035f924f4f8c33e8a9cecef
TuanTran <tuan072005@gmail.com>
2025-12-16 23:17:55 +0700

    chỉnh sửa

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,5 +8,12 @@ urlpatterns = [
     path("verify_code/", views.verify_code, name="verify_code"),
     path("finish_signup/", views.finish_signup, name="finish_signup"),
     path("create_name_pass/", views.create_name_pass, name="create_name_pass"),
-
+    path("verify_acc/", views.verify_acc, name="verify_acc"),
+    path("sitepersonal/",views.SitePerSonal,name="sitepersonal"),
+    path("setting/",views.setting,name="setting"),
+    path("hoatdong/",views.action,name="action"),
+    path("the/",views.card,name="cards"),
+    path("boadsPersonal/",views.boardspersonal,name="boards"),
+    path("members/",views.members,name="members"),
+    path('api/send-otp/', views.send_otp_api, name='send_otp_api'),
 ]
