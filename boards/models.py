4ad8db66bd135fe08eecda2cc559697b33f3aa7f
Pham Thanh An-23130002 <23130002@st.hcmuaf.edu.vn>
2025-12-23 18:51:42 +0700

    Tạo liên kết chia sẻ bảng

--- boards/models.py
+++ boards/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from accounts.models import User
+import uuid
 
 class Board(models.Model):
     name = models.CharField(max_length=50)
@@ -9,11 +10,11 @@ class Board(models.Model):
         ('public', 'Công khai'),
     ]
     visibility = models.CharField(max_length=20, choices=VISIBILITY_CHOICES, default='workspace')
-    
     description = models.TextField(max_length=200, blank=True, null=True) 
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owned_boards')
-    
     created_at = models.DateTimeField(auto_now_add=True)
+    #Tạo mã chia sẻ ngẫu nhiên, không trùng lặp
+    share_token = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
 
     def __str__(self):
         return self.name
