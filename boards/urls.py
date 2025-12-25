7ab63164f34ff9e8f98e6ec7661475923448855c
Pham Thanh An-23130002 <23130002@st.hcmuaf.edu.vn>
2025-12-25 11:54:06 +0700

    fix

--- boards/urls.py
+++ boards/urls.py
@@ -21,6 +21,5 @@ urlpatterns = [
     path("api/checklist/item/add/", views.add_checklist_item, name="add_checklist_item"),
     path("api/checklist/item/toggle/", views.toggle_checklist_item, name="toggle_checklist_item"),
     path("api/checklist/delete/", views.delete_checklist, name="delete_checklist"),
-
-
+    path('api/card/assign_member/', views.assign_member_to_card, name='assign_member_to_card'),
 ]
